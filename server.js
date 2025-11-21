3cf85f8468070cb7a49158198e757eddfac1261b
Atabey <hesaphesapenesbatur@gmail.com>
2025-11-21 21:23:41 +0300

    YerliDiscord

--- server.js
+++ server.js
@@ -18,16 +18,26 @@ app.get("/channels", (req, res) => {
     res.json(channels);
 });
 
+// Kullanıcı listesi route'u (frontend için)
+app.get("/users", (req, res) => {
+    res.json(Object.keys(users));
+});
+
 // Kanal bazlı mesaj hafızası
 const messages = {}; // { channelId: [ { text, user, time } ] }
+// DM / Grup DM hafızası
+const dms = {}; // { key: [ { text, from, time, users } ] }
 
 // Kullanıcı listesi (username -> socket.id)
 const users = {};
 
+// Sesli sohbet kullanıcıları (username -> socket.id)
+const voiceUsers = {};
+
 io.on("connection", (socket) => {
     console.log("Bir kullanıcı bağlandı.");
 
-    // Kullanıcı adını ayarla
+    // Kullanıcı adını kaydet
     socket.on("register", (username) => {
         socket.username = username;
         users[username] = socket.id;
@@ -41,15 +51,25 @@ io.on("connection", (socket) => {
         }
     });
 
-    // Mesaj gönder
+    // Mesaj gönderme
     socket.on("message", (msg) => {
         if(msg.dmUser){
-            // DM mesajı: sadece gönderici ve alıcı görür
+            // DM mesajı: sadece gönderen ve alıcı görür
             const toSocketId = users[msg.dmUser];
             const fromSocketId = socket.id;
 
             if(toSocketId) io.to(toSocketId).emit("message", msg);
-            io.to(fromSocketId).emit("message", msg); // göndericiye de gönder
+            io.to(fromSocketId).emit("message", msg); // gönderen görsün
+        } else if(msg.groupUsers){
+            // Grup DM mesajı
+            const key = msg.groupUsers.sort().join("-");
+            if(!dms[key]) dms[key] = [];
+            dms[key].push(msg);
+
+            msg.groupUsers.forEach(u => {
+                const userSocketId = users[u];
+                if(userSocketId) io.to(userSocketId).emit("message", msg);
+            });
         } else {
             // Kanal mesajı: tüm kullanıcılar görür
             if(!messages[msg.channel]) messages[msg.channel] = [];
@@ -58,9 +78,24 @@ io.on("connection", (socket) => {
         }
     });
 
+    // SESLİ SOHBET (WebRTC) EVENTLERİ
+    socket.on("voice-join", ({ username }) => {
+        voiceUsers[username] = socket.id;
+    });
+
+    socket.on("voice-signal", ({ to, from, signal }) => {
+        const toSocket = voiceUsers[to];
+        if(toSocket){
+            io.to(toSocket).emit("voice-signal", { from, signal });
+        }
+    });
+
     // Kullanıcı ayrılınca listeden sil
     socket.on("disconnect", () => {
-        if(socket.username) delete users[socket.username];
+        if(socket.username) {
+            delete users[socket.username];
+            delete voiceUsers[socket.username];
+        }
         console.log(`${socket.username || "Anonim"} ayrıldı`);
     });
 });
